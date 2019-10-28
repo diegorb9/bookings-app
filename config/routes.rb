779e160a9f3fb7036066c33f6c5c60e30489085f
Diego Oliveira <diegoitarare@gmail.com>
2019-10-27 23:49:19 -0300

    Implements users sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :bookings
+
+  root 'bookings#index'
 end
---
+++ spec/features/users/sign_up_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User creates an account' do
+  before { visit new_user_registration_path }
+
+  scenario 'fills the fields correctly and show success message' do
+    fill_in 'Nome', with: 'John Doe'
+    fill_in 'E-mail', with: 'john@example.com'
+    fill_in 'Senha', with: 'foobar123'
+    fill_in 'Confirmação da Senha', with: 'foobar123'
+
+    click_button 'Cadastrar'
+
+    expect(page).to have_text('Bem-vindo! Você se registrou com êxito.')
+  end
+
+  scenario "doesn't fill the fields and show error message " do
+    click_button 'Cadastrar'
+
+    expect(page).to have_text('E-mail não pode ficar em branco') &&
+                    have_text('Senha não pode ficar em branco') &&
+                    have_text('Nome não pode ficar em branco')
+  end
+end
