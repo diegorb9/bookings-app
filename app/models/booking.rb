65b8e54ad978e7ec6c7bb0a3e5b4bd4fc4a8cfdb
Diego Oliveira <diegoitarare@gmail.com>
2019-10-28 22:24:44 -0300

    Implements uniqueness validation to Bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   validates :description, :date, :time, presence: true
+  validates :date, uniqueness: {
+    scope: :time, message: I18n.t('.errors.messages.already_reserved')
+  }
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -11,5 +11,21 @@ RSpec.describe Booking, type: :model do
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_presence_of(:date) }
     it { is_expected.to validate_presence_of(:time) }
+
+    describe 'uniqueness_of booking' do
+      let(:date) { '30/10/2019' }
+      let(:user) { create(:user) }
+
+      before { create(:booking, date: date, user: user) }
+
+      it 'returns error message' do
+        booking = build(:booking, date: date, user: user)
+        booking.valid?
+
+        expect(
+          booking.errors[:date]
+        ).to eql(['já foi reservada para o horário escolhido'])
+      end
+    end
   end
 end
