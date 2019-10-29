62327c91e57724a33bc6adcfdb9f102b66d14d15
Diego Oliveira <diegoitarare@gmail.com>
2019-10-28 21:16:00 -0300

    Implements bookings creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 class BookingsController < ApplicationController
+  respond_to :html, :js
+
   before_action :authenticate_user!
+
+  def new
+    @booking = current_user.bookings.new
+  end
+
+  def create
+    @booking = current_user.bookings.create(booking_params)
+
+    respond_with @booking, location: bookings_path
+  end
 
   def index
     @bookings_presenter = BookingsPresenter.new(
       Booking.order(date: :asc),
       params[:date] || Date.today
     )
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :time, :description)
   end
 end
---
+++ spec/features/bookings/create_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Creating bookings', js: true do
+  include_context 'Authenticated user'
+
+  given!(:user) { create(:user) }
+
+  background { visit bookings_path }
+
+  before { Timecop.freeze('30/10/2019') }
+  after { Timecop.return }
+
+  scenario 'opens modal, fills description and checks for success message' do
+    within 'tr:nth-child(2) td:nth-child(2)' do
+      click_link 'Disponível'
+    end
+
+    within '#modal' do
+      expect(page).to have_text('Data') && have_text('28/10/2019')
+      expect(page).to have_text('Horário') && have_text('07:00:00')
+
+      fill_in 'Descrição', with: 'Example'
+
+      click_button 'Reservar'
+    end
+
+    expect(page).to have_text('Reserva realizada com sucesso.')
+
+    within 'tr:nth-child(2) td:nth-child(2)' do
+      expect(page).to have_text('Reservado para John Doe')
+    end
+  end
+
+  scenario 'opens the modal, clicks cancel and checks if the reservation available' do
+    within 'tr:nth-child(2) td:nth-child(2)' do
+      click_link 'Disponível'
+    end
+
+    within '#modal' do
+      expect(page).to have_text('Data') && have_text('28/10/2019')
+      expect(page).to have_text('Horário') && have_text('07:00:00')
+
+      fill_in 'Descrição', with: 'Example'
+
+      click_button 'Fechar'
+    end
+
+    expect(page).not_to have_text('Reserva realizada com sucesso.')
+
+    within 'tr:nth-child(2) td:nth-child(2)' do
+      expect(page).not_to have_text('Reservado para John Doe')
+      expect(page).to have_text('Disponível')
+    end
+  end
+end
